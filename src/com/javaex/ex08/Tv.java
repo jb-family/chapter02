a5db79469b16f305f349e97048d4fad46cc33c06
jongbin-choi <kooma1123@naver.com>
2022-04-23 19:19:07 +0900

    2022-04-23 Tv조작

--- src/com/javaex/ex08/Tv.java
+++ src/com/javaex/ex08/Tv.java
@@ -1,5 +1,129 @@
 package com.javaex.ex08;
 
 public class Tv {
+	
+	//필드
+	private int channel;
+	private int volume;
+	private boolean power;
 
+	
+	//생성자
+	public Tv() {
+	
+	}
+	
+	public Tv(int channel, int volume, boolean power) {
+		this.channel = channel;
+		this.volume = volume;
+		this.power = power;
+	}
+	
+	
+	
+	//메소드 - gs
+	
+	public int getChannel() {
+		return channel;
+	}
+	
+	public int getVolume() {
+		return volume;
+	}
+	
+	public boolean getPower() {
+		return power;
+	}
+	
+	
+	
+	//메소드 - 일반
+	
+	public void status() {
+		System.out.println("채널 :" + channel + " 볼륨 : " + volume + " 전원상태 : " + power);
+	}
+	
+	public void power(boolean on) {
+		if(on == true) {
+			this.power = true;
+			System.out.println("전원이 켜졌습니다.");
+		}else if(on == false) {
+			this.power = false;
+			if(this.power == false) {
+				channel = 7;
+				volume = 20;
+			}
+			System.out.println("전원이 꺼졌습니다.");
+		}
+	}
+	
+	public void channel (int channel) {
+		if(power == true && channel > 0 && channel < 256) {
+		if(channel > this.channel) {
+			this.channel = channel;
+		}
+		else if(channel < this.channel) {
+			this.channel = channel;
+			}
+		}else if (channel < 1 || channel > 255) {
+			System.out.println("없는 채널입니다.");
+		}
+		else {
+			System.out.println("Tv가 꺼져있어 조작할 수 없습니다.");
+		}
+		
+	}
+	
+	public void channel (boolean up) {
+		if(power == true) {
+		if(up == true && channel > 255) {
+			channel++;
+		}else if(up == false && channel > 1) {
+			channel--;
+		}
+		else {
+			System.out.println("없는 채널입니다.");
+		}
+		}else {
+			System.out.println("Tv가 꺼져있어 조작할 수 없습니다.");
+		}
+	}
+	
+	public void volume (int volume) {
+		if(power == true) {
+			if(volume > this.volume && volume <= 100) {
+				this.volume = volume;
+			}else if(volume < this.volume && volume >= 0) {
+				this.volume = volume;
+				if(volume == 0) {
+					System.out.println("현재는 음소거상태입니다.");
+				}
+			}else if (volume == 0) {
+				System.out.println("현재는 음소거상태입니다.");
+			}
+			else if (volume > 100) {
+				this.volume = 100;
+			}
+		}
+		else {
+			System.out.println("Tv가 꺼져있어 조작할 수 없습니다.");
+		}
+	}
+	
+	public void volume (boolean up) {
+		if(up == true && this.volume >= 0 && this.volume < 100) { 
+			this.volume++;
+		}
+		else if(up == false && this.volume > 0 && this.volume <= 100) {
+		this.volume--; 
+		if(this.volume == 0) {
+			System.out.println("현재는 음소거상태입니다.");
+		}
+		}
+	}
+	
+	
+	// 끝
+	
+	
 }
--- src/com/javaex/ex08/TvApp.java
+++ src/com/javaex/ex08/TvApp.java
@@ -4,6 +4,72 @@ public class TvApp {
 
 	public static void main(String[] args) {
 
+		Tv tv = new Tv(7, 20, false);
+		tv.status();		// 초기 값
+		
+		System.out.println();
+		System.out.println("=========볼륨 테스트=======");
+		System.out.println();
+		tv.volume(100);		// 볼륨 100 설정
+		tv.status();		// tv 꺼진상태 조작불가
+		
+		System.out.println();
+		System.out.println("========채널 테스트========");
+		System.out.println();
+		tv.channel(100);	// 채널 100 설정
+		tv.status();		// tv 꺼진상태 조작불가
+		
+		System.out.println();
+		System.out.println("========전원테스트========");
+		System.out.println();
+		tv.power(true);		// tv 전원 on
+		tv.status();		// tv 켜진상태 조작가능
+		
+		System.out.println();
+		System.out.println("=========채널테스트========");
+		System.out.println();
+		
+		tv.channel(1);		// tv 채널7 -> 1이동
+		tv.status();		// tv 채널 1
+		tv.channel(0);		// tv 채널 1 -> 0이동
+		tv.status();		// tv 채널이동불가
+		tv.channel(255);	// tv 채널 1 -> 255이동
+		tv.status();		// tv 채널 255
+		tv.channel(256);	// tv 채널 255 -> 256이동
+		tv.status();		// tv 채널이동불가
+		tv.channel(true);	// 채널 1증가
+		tv.status();		// tv 채널이동불가
+		tv.channel(1);		// tv 채널 255 -> 1이동
+		tv.status();		// 채널 1
+		tv.channel(false);	// 채널 1감소
+		tv.status();		// tv 채널이동불가
+		
+		System.out.println("==========볼륨테스트=========");
+		tv.volume(50);		// 볼륨 20 -> 50증가
+		tv.status();		// 볼륨 50
+		tv.volume(101);		// 볼륨 50 -> 101이동 (100으로 조정)
+		tv.status();		// tv 볼륨증가불가
+		tv.volume(1);		// 볼륨 100 -> 1이동
+		tv.status();		// 볼륨 1
+		tv.volume(0);		// 볼륨 1 -> 0이동
+		tv.status();		// 볼륨 0 음소거상태
+		tv.volume(120);		// 볼륨 1 -> 120이동
+		tv.status();		// 볼륨 100으로 조정
+		tv.volume(true);	// 볼륨 100 -> 101증가 (100으로 조정)
+		tv.status();		// tv 볼륨증가불가 
+		tv.volume(1);		// tv볼륨 100 -> 1감소
+		tv.status();		// 볼륨 1
+		tv.volume(false);	// 볼륨 1 -> 0감소
+		tv.status();
+		
+		System.out.println("==========전원테스트==========");
+		tv.power(false);
+		tv.status();
+		
+		
+		
+		
+		
 		
 		
 		
